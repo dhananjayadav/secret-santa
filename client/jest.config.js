d9bab6d12ab9c3b6e2d112b9b19abc5068e0bb8b
dhananjayadav <dhananjay2451@gmail.com>
2025-04-05 22:01:08 +0800

    update client jest configuration

---
+++ client/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    transform: {
+      '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+  };
+  
