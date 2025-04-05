87ff9f21eea148e34646321f99bd465e5edfbaca
dhananjayadav <dhananjay2451@gmail.com>
2025-04-05 19:48:07 +0800

    install express fileupload and cors

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const indexRouter = require("./routes");
 const app = express();
 const PORT = 9000;
 
+app.use(cors());
+app.use(fileUpload());
+
 app.use("/api", indexRouter);
 
 app.listen(PORT, () => {
