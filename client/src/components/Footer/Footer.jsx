4022be1e123e3ffe7c435e6a4bfaab8380721f57
dhananjayadav <dhananjay2451@gmail.com>
2025-04-05 20:10:31 +0800

    create footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p className="footer-text">
+        This page is created by <a href="mailto:dhananjay@gmail.com" className="footer-link">dhananjay2451@gmail.com</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
